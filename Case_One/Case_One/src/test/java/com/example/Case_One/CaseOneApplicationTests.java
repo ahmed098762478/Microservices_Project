1891672d6a089627cca99cba0599678183c212f6
ahmed098762478 <162605372+ahmed098762478@users.noreply.github.com>
2025-01-18 22:23:07 +0100

    1er commit

---
+++ Case_One/Case_One/src/test/java/com/example/Case_One/CaseOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Case_One;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
