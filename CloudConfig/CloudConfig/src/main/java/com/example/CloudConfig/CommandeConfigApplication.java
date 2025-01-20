b679eabf0b63a2129be151be8233a48f7bd73040
ahmed098762478 <162605372+ahmed098762478@users.noreply.github.com>
2025-01-20 14:27:36 +0100

    Cloud configuration

---
+++ CloudConfig/CloudConfig/src/main/java/com/example/CloudConfig/CommandeConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandeConfigApplication.class, args);
+	}
+
+}
